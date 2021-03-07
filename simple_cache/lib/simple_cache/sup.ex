114ba618c5ff22b6b0b2e777294ccdcb04d365f9
Alexey Zlobin <alexey.zlobin@gmail.com>
2021-03-07 23:52:11 +0300

    TCP server stub

--- simple_cache/lib/simple_cache/sup.ex
+++ simple_cache/lib/simple_cache/sup.ex
@@ -1,7 +1,7 @@
 defmodule SimpleCache.Sup do
   alias SimpleCache.Element
 
-  use DynamicSupervisor
+  use DynamicSupervisor, restart: :permanent
 
   @spec start_child(term(), pos_integer()) :: DynamicSupervisor.on_start_child()
   def start_child(value, lease_time) do
@@ -10,16 +10,8 @@ defmodule SimpleCache.Sup do
       start: {Element, :start_link, [value, lease_time]},
       restart: :temporary
     }
-    DynamicSupervisor.start_child(__MODULE__, spec)
-  end
 
-  @spec child_spec(term()) :: Supervisor.child_spec()
-  def child_spec(arg) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, [arg]},
-      restart: :permanent
-    }
+    DynamicSupervisor.start_child(__MODULE__, spec)
   end
 
   @spec start_link(term()) :: Supervisor.on_start()
