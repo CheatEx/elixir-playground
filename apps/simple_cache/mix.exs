c93ac5c2b437263c48b9dca44a80ae206a8da594
Alexey Zlobin <alexey.zlobin@gmail.com>
2021-03-14 20:36:04 +0300

    to umbrella

--- apps/simple_cache/mix.exs
+++ apps/simple_cache/mix.exs
@@ -6,6 +6,10 @@ defmodule SimpleCache.MixProject do
       app: :simple_cache,
       version: "0.1.0",
       elixir: "~> 1.11",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
